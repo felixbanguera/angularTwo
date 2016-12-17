a9e3e9ab8e30539e8c04f5e40aa571889cfd4cf9
Jorge Banguera <jorge.banguera@YUXIDEV-JBANGUERA-2.local>
2016-12-17 08:26:34 -0500

    Lazy load explained, this is routing in modules, routes for child also

---
+++ apps/lazy-load-ex/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { AppRoutingModule } from './app-routing.module';
+import { NewsModule } from './news/news.module';
+import { HomeModule } from './home/home.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HomeModule,
+    NewsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
